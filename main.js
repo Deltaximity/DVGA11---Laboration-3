7c2a93ba492dcc47f0fd298316e437dc4e94002f
Deltaximity <45205838+Deltaximity@users.noreply.github.com>
2023-05-19 17:31:22 +0200

    Fixat controls
    Lade till controls med eventlisteners för att öka/minsta antalet på föremål

--- main.js
+++ main.js
@@ -25,10 +25,48 @@ window.addEventListener('load', () => {
                 html = html.substring(0, html.length - 2); // tar bort ", "
                 html += `</p>`;
             }
-            html += `</div></div>`;
+            html += `</div><div class="controls"></div></div>`;
         });
 
         html += `</div>`;
         container.insertAdjacentHTML("beforeend", html);
     }
+
+    // CONTROLS
+    let controls = document.querySelectorAll('.controls');
+    controls.forEach(control => {
+        let quantity = document.createElement('div');
+        let removeBtn = document.createElement('div');
+        let addBtn = document.createElement('div');
+        let quantityTextNode = document.createTextNode('0');
+        let removeBtnTextNode = document.createTextNode('-');
+        let addBtnTextNode = document.createTextNode('+');
+        quantity.appendChild(quantityTextNode);
+        removeBtn.appendChild(removeBtnTextNode);
+        addBtn.appendChild(addBtnTextNode);
+        quantity.classList.add('quantity');
+        removeBtn.classList.add('remove');
+        addBtn.classList.add('add');
+        control.appendChild(removeBtn);
+        control.appendChild(quantity);
+        control.appendChild(addBtn);
+        // quantity.contentEditable = true; // Kan vara något bra att implementera, men inget krav
+
+        // FUNKTIONER
+        addBtn.addEventListener('click', () => {
+            let num = quantity.textContent;
+            if (num >= 99) return;
+            quantity.textContent = ++num;
+        });
+
+        removeBtn.addEventListener('click', () => {
+            let num = quantity.textContent;
+            if (num <= 0) return;
+            quantity.textContent = --num;
+        });
+    });
 });
+
+// SKRIV UT CONTROLS + EVENT LISTENERS ✅
+// HÄMTA PRIS PÅ FÖREMÅL MED BUBBLE CAPTURING
+// SKRIV "SLUTFÖR ORDER" FUNKTIONALITET
